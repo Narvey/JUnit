4176ce326102ee67742170fb4ab8d4dab01456f5
N Harvey <nharvey@mymail.mines.edu>
2012-09-28 17:25:23 -0600

    First commit after adding Connor

--- src/classes/Cashier.java
+++ src/classes/Cashier.java
@@ -1,7 +1,9 @@
 package classes;
 
 public class Cashier {
-	public Envelope makeChange(Envelope paid, int dollars, int cents){
+	public static Envelope makeChange(Envelope paid, int dollars, int cents){
+		//TODO Connor, what do you think of this being static? I didn't see the need to declare a Cashier ever.
+		// 
 		return new Envelope();
 	}
 }
